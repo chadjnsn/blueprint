6d74ee5d41d521d6b1b0822afb4a2b52bf8a481f
Chad Johnson <chadjnsn@gmail.com>
2016-03-26 22:44:19 -0700

    Remove sleuth percentage sampler
    Spring is using a percentage sampler by default. It might be worth
    adding another one, just to show how it can be done, but it's not really
    needed. This could be covered by documentation.

--- src/main/java/xyz/chadjohnson/DemoApplication.java
+++ src/main/java/xyz/chadjohnson/DemoApplication.java
@@ -1,14 +1,9 @@
 package xyz.chadjohnson;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.sleuth.sampler.PercentageBasedSampler;
-import org.springframework.cloud.sleuth.sampler.SamplerProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -18,17 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 //@EnableEurekaClient
 public class DemoApplication {
 
-	@Value("${spring.sleuth.sampler.percentage}")
-	private float samplePercentage;
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@Bean
-	public PercentageBasedSampler defaultSampler() {
-		SamplerProperties samplerProperties = new SamplerProperties();
-		samplerProperties.setPercentage(samplePercentage);
-		return new PercentageBasedSampler(samplerProperties);
 	}
 }
